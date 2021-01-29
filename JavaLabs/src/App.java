3ea9ab481acad3764e3ad4f24d2d7bcaa5470724
agnusha <agnia2000agnia>
2021-01-29 17:07:58 +0300

    feat: finish task 1

--- JavaLabs/src/App.java
+++ JavaLabs/src/App.java
@@ -1,5 +1,19 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        // task 1
+        System.out.println("Silence is golden");
+
+        SimpleDateFormat formater = new SimpleDateFormat("EEEE");
+        Date date = new Date();
+
+        System.out.println(formater.format(date));
+        System.out.println("Agnia");
+
+        // task 2
+
     }
 }

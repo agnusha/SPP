d387ba874358962fb10944254f3b0f606076968e
agnusha <agnia2000agnia>
2021-01-29 17:40:57 +0300

    feat: task 3

--- JavaLabs/src/App.java
+++ JavaLabs/src/App.java
@@ -1,5 +1,6 @@
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -15,7 +16,17 @@ public class App {
 
         // task 2
         Float result = (float) 1 / 2 + (float) 1 / 4;
-        System.out.println("Result:" + result);
+        System.out.println("Result: " + result);
 
+        // task 3
+        var input = new Scanner(System.in).nextInt();
+        if (input > 3) {
+            System.out.println("Add 10");
+            input += 10;
+        } else {
+            System.out.println("Subtract 10");
+            input -= 10;
+        }
+        System.out.println("Result: " + input);
     }
 }

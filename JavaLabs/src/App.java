24b921f561387466b3424ad980ae7212975a30a5
AgniaStaravoitava <agnia2000@inbox.ru>
2021-06-19 22:03:36 +0300

    feat: insert values

--- JavaLabs/src/App.java
+++ JavaLabs/src/App.java
@@ -5,28 +5,12 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        // task 1
-        System.out.println("Silence is golden");
+        while (true) {
+            System.out.println("Insert first element of pair");
+            var a = Integer.parseInt(System.console().readLine());
 
-        SimpleDateFormat formater = new SimpleDateFormat("EEEE");
-        Date date = new Date();
-
-        System.out.println(formater.format(date));
-        System.out.println("Agnia");
-
-        // task 2
-        Float result = (float) 1 / 2 + (float) 1 / 4;
-        System.out.println("Result: " + result);
-
-        // task 3
-        var input = new Scanner(System.in).nextInt();
-        if (input > 3) {
-            System.out.println("Add 10");
-            input += 10;
-        } else {
-            System.out.println("Subtract 10");
-            input -= 10;
+            System.out.println("Insert second element of pair");
+            var b = Integer.parseInt(System.console().readLine());
         }
-        System.out.println("Result: " + input);
     }
 }

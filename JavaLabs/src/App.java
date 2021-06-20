2f6e2b775be7b0a198c24299cd072bb6d65b9d38
AgniaStaravoitava <agnia2000@inbox.ru>
2021-06-20 11:51:21 +0300

    feat: finish lab1

--- JavaLabs/src/App.java
+++ JavaLabs/src/App.java
@@ -1,16 +1,29 @@
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Scanner;
-
 public class App {
     public static void main(String[] args) throws Exception {
+        System.out.println("Insert max element count");
+        var n = Integer.parseInt(System.console().readLine());
+
+        int[] list = new int[n];
+        for (int i = 0; i < n; i++)
+            list[i] = i;
 
         while (true) {
             System.out.println("Insert first element of pair");
-            var a = Integer.parseInt(System.console().readLine());
+            var p = Integer.parseInt(System.console().readLine());
 
             System.out.println("Insert second element of pair");
-            var b = Integer.parseInt(System.console().readLine());
+            var q = Integer.parseInt(System.console().readLine());
+
+            if (list[p] == list[q]) {
+                System.out.println("Don't added a pair: " + p + "-" + q);
+                continue;
+            }
+
+            for (int i = 0; i < n; i++)
+                if (list[i] == list[p])
+                    list[i] = list[q];
+
+            System.out.println("Added a pair: " + p + "-" + q);
         }
     }
 }
